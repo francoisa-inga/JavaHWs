79bb63129e85b8565574d35d453935eb21bb497f
Inga <inga_ai@yahoo.com>
2019-12-26 16:04:30 +0200

    HomeWork 25/12/2019: Calculation of repeated elements and making Array in revert order; Array Runner contains 2 tasks so far ready

--- src/ArraysExercises/ArrayRepeatedNumbersV1Arraylist.java
+++ src/ArraysExercises/ArrayRepeatedNumbersV1Arraylist.java
@@ -1,6 +1,8 @@
 package ArraysExercises;
 
-public class ArrayRepeatedNumbers {
+import java.util.ArrayList;
+
+public class ArrayRepeatedNumbersV1Arraylist {
 
     public void displayRepeatedElements(int[] numbers){
         int i,j;
@@ -10,6 +12,9 @@ public class ArrayRepeatedNumbers {
         for(i=0;i<numbersStore.length;i++) {
             numbersStore[i] = 1;
         }
+
+        //Create an ArrayList to store the repeated elements
+        ArrayList<Integer>resultRepeatedArray = new ArrayList<Integer>();
 
         // Comparison of elements, searching for repeats
         for(i=0;i< numbers.length;i++) {
@@ -21,12 +26,18 @@ public class ArrayRepeatedNumbers {
             }
         }
 
-            // Print repeated elements
+            // Fill Arraylist of repeated elements
             for (i = 0; i < numbersStore.length; i++) {
                 if (numbersStore[i] > 1) {
-                    System.out.println("The element [" + i + "] =" + numbers[i] + ", repeated " + numbersStore[i] + " times");
+                    resultRepeatedArray.add(numbers[i]);
                 }
             }
+
+        // Print repeated elements
+                for(i=0;i<resultRepeatedArray.size();i++){
+                    System.out.println("Element of the result array #"+i+" = "+resultRepeatedArray.get(i));
+                }
+
  //       }
     }
 }
--- src/ArraysExercises/ArrayRunner.java
+++ src/ArraysExercises/ArrayRunner.java
@@ -3,10 +3,16 @@ package ArraysExercises;
 public class ArrayRunner {
     public static void main(String[] args) {
         //Create and initialize array
-        ArrayRepeatedNumbers arrayMain = new ArrayRepeatedNumbers();
+        ArrayRepeatedNumbersV1Arraylist arrayMain = new ArrayRepeatedNumbersV1Arraylist();
         int[] numbersMainArray = {1,1,1,4,5,5,7,8,9,5};
 
+        /** Temporarily disabled, under debugging (another variant of Task displaying the duplicated elements)
+        int[] numbersMainArray = {0,0,7,5,5,7,9,9};
+        ArrayRepeatedNumbersV2Array arrayMainArray = new ArrayRepeatedNumbersV2Array();
+         */
+
         //Calculate repeated elements
+       // arrayMain.displayRepeatedElements(numbersMainArray);
         arrayMain.displayRepeatedElements(numbersMainArray);
 
         //Display array in revert order
